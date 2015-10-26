8a2dbb993797a46fa9420dacc3e0c77311110ba8
cenjianjun3730 <cenjianjun3730@sina.com>
2015-10-26 16:12:17 +0800

    删除缓存

--- App/Runtime/Data/_fields/lqsh.dynamic.php
+++
@@ -1,1 +0,0 @@
-<?php	return array ( 0 => 'id', 1 => 'dynamic_name', 2 => 'dynamic_img', 3 => 'dynamic_content', 4 => 'dynamic_classify', 5 => 'time', '_autoinc' => true, '_pk' => 'id', '_type' => array ( 'id' => 'int(11)', 'dynamic_name' => 'varchar(20)', 'dynamic_img' => 'varchar(250)', 'dynamic_content' => 'text', 'dynamic_classify' => 'varchar(10)', 'time' => 'int(10)', ), );?>
